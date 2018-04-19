6e124bf2b528db5637d5834fdd06697984367eac
Pierre Fenoll <pierrefenoll@gmail.com>
2018-04-19 10:14:11 +0200

    master: moar tests

--- test/otp_vsn_testing_tests.erl
+++ test/otp_vsn_testing_tests.erl
@@ -4,6 +4,14 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("otp_vsn/include/otp_vsn.hrl").
 
-t_test_() ->
+do_test_() ->
     [?_assertMatch([_|_], ?OTP_VSN)
+
+    ,?_assert(is_integer(?OTP_VSN_MAJOR) andalso ?OTP_VSN_MAJOR > 0)
+    ,?_assert(is_integer(?OTP_VSN_MINOR) andalso ?OTP_VSN_MINOR >= 0)
+    ,?_assert(is_integer(?OTP_VSN_PATCH) andalso ?OTP_VSN_PATCH >= 0)
+
+    ,?_assertMatch([_|_], ?OTP_VSN_MAJOR_STRING)
+    ,?_assertMatch([_|_], ?OTP_VSN_MINOR_STRING)
+    ,?_assertMatch([_|_], ?OTP_VSN_PATCH_STRING)
     ].
